98a11d42f7130d6705f7f9bac516b412bd57dc3e
Tomoya <fujita.t.2016c@gmail.com>
2021-09-22 14:42:23 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
+# install vimdot
+curl "https://raw.githubusercontent.com/TomoyaFujita2016/mini-dotfiles/main/install_vim.sh" | sh
 
+# install bashdot
+curl "https://raw.githubusercontent.com/TomoyaFujita2016/mini-dotfiles/main/install_bash.sh" | sh
